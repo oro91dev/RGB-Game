76366fb99f7be2c66047cfa518e81cb4029d9572
OroDev <oro91dev@gmail.com>
2018-10-17 22:45:10 +0200

    Added colorGame functionality

---
+++ colorGame.js
@@ -0,0 +1,42 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+
+]
+
+let squares = document.querySelectorAll(".square");
+let pickedColor = pickColor();
+let colorDisplay = document.getElementById("colorDisplay");
+let messageDisplay = document.querySelector("#message");
+colorDisplay.textContent = pickedColor;
+
+for(let i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+
+    squares[i].addEventListener("click", function() {
+        let clickedColor = this.style.backgroundColor;
+
+        if(clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
+        }else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try again!"
+        }
+    });
+}
+
+function changeColors(color) {
+    for(let i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random]; 
+}
