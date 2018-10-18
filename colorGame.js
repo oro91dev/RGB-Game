5b73e7e082306daf47364a0f2f7ae15cf229cbe2
OroDev <oro91dev@gmail.com>
2018-10-18 23:05:09 +0200

    added random color array
    removed hardcoded array from colors

--- colorGame.js
+++ colorGame.js
@@ -1,17 +1,13 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
+var colors = generateRandomColors(6);
 
-]
+
 
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
+let h1 = document.querySelector("h1");
+
 colorDisplay.textContent = pickedColor;
 
 for(let i = 0; i < squares.length; i++){
@@ -23,6 +19,7 @@ for(let i = 0; i < squares.length; i++){
         if(clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         }else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try again!"
@@ -39,4 +36,20 @@ function changeColors(color) {
 function pickColor() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random]; 
+}
+
+function generateRandomColors(num){
+    var arr = [];
+
+    for(var i= 0; i < num; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
